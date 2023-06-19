05c0c2460d4d921e1d5de562aa99cd65be21ee4b
MateusAra <mateus19araujo@hotmail.com>
2023-06-19 20:27:20 -0300

    Extract method - 3

--- Academia Master/src/control/Validador.java
+++ Academia Master/src/control/Validador.java
@@ -419,64 +419,62 @@ public class Validador {
         } 
     }
     
-    private static boolean isValidCnpj(String cnpj){
-        
-        if (cnpj.equals("00000000000000") || 
+    private static boolean isInvalidCnpjDigits(String cnpj) {
+        return cnpj.equals("00000000000000") ||
             cnpj.equals("11111111111111") ||
-            cnpj.equals("22222222222222") || 
+            cnpj.equals("22222222222222") ||
             cnpj.equals("33333333333333") ||
-            cnpj.equals("44444444444444") || 
+            cnpj.equals("44444444444444") ||
             cnpj.equals("55555555555555") ||
-            cnpj.equals("66666666666666") || 
+            cnpj.equals("66666666666666") ||
             cnpj.equals("77777777777777") ||
-            cnpj.equals("88888888888888") || 
-            cnpj.equals("99999999999999") ||
-            cnpj.length() != 14)
-            return(false);
+            cnpj.equals("88888888888888") ||
+            cnpj.equals("99999999999999");
+    }
 
-        char dig13, dig14;
-        int sm, i, r, num, peso;
+    private static char calculateDigit(String cnpj, int position) {
+        int sm = 0;
+        int peso = 2;
+        int num;
+
+        for (int i = position - 1; i >= 0; i--) {
+            num = cnpj.charAt(i) - '0';
+            sm += num * peso;
+            peso++;
+            if (peso == 10) {
+                peso = 2;
+            }
+        }
+
+        int r = sm % 11;
+        char digit;
+        if (r == 0 || r == 1) {
+            digit = '0';
+        } else {
+            digit = (char) ((11 - r) + '0');
+        }
+
+        return digit;
+    }
+
+    private static boolean isInvalidCnpjFormat(String cnpj) {
+        return cnpj.length() != 14;
+    }
+
+    private static boolean isValidCnpj(String cnpj) {
+        if (isInvalidCnpjFormat(cnpj) || isInvalidCnpjDigits(cnpj)) {
+            return false;
+        }
 
         try {
-            // Calculo do primeiro Digito Verificador
-            sm = 0;
-            peso = 2;
-            for (i=11; i>=0; i--) {
-                num = (int)(cnpj.charAt(i) - 48);
-                sm = sm + (num * peso);
-                peso = peso + 1;
-                if (peso == 10)
-                    peso = 2;
-            }
+            char dig13 = calculateDigit(cnpj, 12);
+            char dig14 = calculateDigit(cnpj, 13);
 
-            r = sm % 11;
-            if ((r == 0) || (r == 1))
-                dig13 = '0';
-            else dig13 = (char)((11-r) + 48);
-
-            // Calculo do segundo Digito Verificador
-            sm = 0;
-            peso = 2;
-            for (i=12; i>=0; i--) {
-                num = (int)(cnpj.charAt(i)- 48);
-                sm = sm + (num * peso);
-                peso = peso + 1;
-                if (peso == 10)
-                    peso = 2;
-            }
-
-            r = sm % 11;
-            if ((r == 0) || (r == 1))
-                dig14 = '0';
-            else dig14 = (char)((11-r) + 48);
-
-            
-            if ((dig13 == cnpj.charAt(12)) && (dig14 == cnpj.charAt(13)))
-                return(true);
-            else
-                return(false);
-        } catch (Exception e) {
-           return(false);
+            return (dig13 == cnpj.charAt(12)) && (dig14 == cnpj.charAt(13));
+        } 
+        catch (Exception e) {
+            return false;
         }
     }
+
 }
