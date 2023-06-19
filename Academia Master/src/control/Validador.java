c5aedeae645d4ecd749b721f26ed07a044f6e854
MateusAra <mateus19araujo@hotmail.com>
2023-06-19 20:46:15 -0300

    Extract Method #3

--- Academia Master/src/control/Validador.java
+++ Academia Master/src/control/Validador.java
@@ -363,61 +363,6 @@ public class Validador {
             label.setText(mensagem);
         }
     }
-  
-    private static boolean isValidCpf(String cpf){
-         
-        if (cpf.equals("00000000000") || 
-            cpf.equals("11111111111") || 
-            cpf.equals("22222222222") || 
-            cpf.equals("33333333333") ||
-            cpf.equals("44444444444") ||
-            cpf.equals("55555555555") ||
-            cpf.equals("66666666666") ||
-            cpf.equals("77777777777") ||
-            cpf.equals("88888888888") ||
-            cpf.equals("99999999999") ||
-            cpf.length() != 11)
-            return(false);
-        
-        char dig10, dig11; 
-        int sm, i, r, num, peso; 
-
-        try { 
-            // Calculo do primeiro Digito Verificador 
-            sm = 0; 
-            peso = 10; 
-            for (i=0; i<9; i++) { 
-                num = (int)(cpf.charAt(i) - 48); 
-                sm = sm + (num * peso); 
-                peso = peso - 1;
-            } 
-            r = 11 - (sm % 11); 
-            if ((r == 10) || (r == 11)) 
-                dig10 = '0'; 
-            else 
-                dig10 = (char)(r + 48); 
-
-            // Calculo do segundo Digito Verificador 
-            sm = 0; 
-            peso = 11; 
-            for(i=0; i<10; i++) { 
-                num = (int)(cpf.charAt(i) - 48);
-                sm = sm + (num * peso); 
-                peso = peso - 1;
-            } 
-            r = 11 - (sm % 11); 
-            if ((r == 10) || (r == 11)) 
-                dig11 = '0'; 
-            else 
-                dig11 = (char)(r + 48); 
-
-            if ((dig10 == cpf.charAt(9)) && (dig11 == cpf.charAt(10))) 
-                return(true); 
-            else return(false);
-        } catch(Exception e) { 
-            return(false); 
-        } 
-    }
     
     private static boolean isInvalidCnpjDigits(String cnpj) {
         return cnpj.equals("00000000000000") ||
@@ -432,7 +377,7 @@ public class Validador {
             cnpj.equals("99999999999999");
     }
 
-    private static char calculateDigit(String cnpj, int position) {
+    private static char calculateDigitCnpj(String cnpj, int position) {
         int sm = 0;
         int peso = 2;
         int num;
@@ -467,8 +412,8 @@ public class Validador {
         }
 
         try {
-            char dig13 = calculateDigit(cnpj, 12);
-            char dig14 = calculateDigit(cnpj, 13);
+            char dig13 = calculateDigitCnpj(cnpj, 12);
+            char dig14 = calculateDigitCnpj(cnpj, 13);
 
             return (dig13 == cnpj.charAt(12)) && (dig14 == cnpj.charAt(13));
         } 
@@ -477,4 +422,57 @@ public class Validador {
         }
     }
 
+    private static char calculateDigitCpf(String cpf, int position) {
+        int sm = 0;
+        int peso = position + 1;
+        int num;
+
+        for (int i = 0; i < position; i++) {
+            num = cpf.charAt(i) - '0';
+            sm += num * peso;
+            peso--;
+        }
+
+        int r = 11 - (sm % 11);
+        char digit;
+        if (r == 10 || r == 11) {
+            digit = '0';
+        } else {
+            digit = (char) (r + '0');
+        }
+
+        return digit;
+    }
+    private static boolean isInvalidCpfDigits(String cpf) {
+        return cpf.equals("00000000000") ||
+            cpf.equals("11111111111") ||
+            cpf.equals("22222222222") ||
+            cpf.equals("33333333333") ||
+            cpf.equals("44444444444") ||
+            cpf.equals("55555555555") ||
+            cpf.equals("66666666666") ||
+            cpf.equals("77777777777") ||
+            cpf.equals("88888888888") ||
+            cpf.equals("99999999999");
+    }
+
+    private static boolean isInvalidCpfFormat(String cpf) {
+        return cpf.length() != 11;
+    }
+
+    private static boolean isValidCpf(String cpf) {
+        if (isInvalidCpfFormat(cpf) || isInvalidCpfDigits(cpf)) {
+            return false;
+        }
+
+        try {
+            char dig10 = calculateDigitCpf(cpf, 9);
+            char dig11 = calculateDigitCpf(cpf, 10);
+
+            return (dig10 == cpf.charAt(9)) && (dig11 == cpf.charAt(10));
+        } 
+        catch (Exception e) {
+            return false;
+        }
+    }
 }
