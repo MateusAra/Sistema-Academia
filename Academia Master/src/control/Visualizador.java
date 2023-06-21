6aace3b725006cab77e4c9b52798955fd27db2c2
MateusAra <mateus19araujo@hotmail.com>
2023-06-21 17:01:18 -0300

    Replace Magic Number with Symbolic Constant #1

--- Academia Master/src/control/Visualizador.java
+++ Academia Master/src/control/Visualizador.java
@@ -1,79 +1,83 @@
 package control;
 
 public class Visualizador {
-    
+    public static final int INDEX_0 = 0;
+    public static final int INDEX_1 = 1;
+    public static final int INDEX_2 = 2;
+    public static final int INDEX_3 = 3;
+    public static final int INDEX_4 = 4;
+    public static final int INDEX_5 = 5;
+    public static final int INDEX_6 = 6;
+    public static final int INDEX_7 = 7;
+    public static final int INDEX_8 = 8;
+    public static final int INDEX_9 = 9;
+    public static final int INDEX_10 = 10;
+    public static final int INDEX_11 = 11;
+    public static final int INDEX_12 = 12;
+    public static final int INDEX_13 = 13;
+
     public static String visualizarPreco(float preco) {
         String string = Float.toString(preco);
-        string = string.replaceAll("\\.",",");
+        string = string.replaceAll("\\.", ",");
         string = "R$ " + string;
         return string;
     }
-    
-    public static final int PRIMEIRO_DIGITO_1 = 0;
-    public static final int PRIMEIRO_DIGITO_2 = 1;
-    public static final int SEGUNDO_DIGITO_1 = 2;
-    public static final int SEGUNDO_DIGITO_2 = 3;
-    public static final int SEGUNDO_DIGITO_3 = 4;
-    public static final int TERCEIRO_DIGITO_1 = 5;
-    public static final int TERCEIRO_DIGITO_2 = 6;
-    public static final int TERCEIRO_DIGITO_3 = 7;
-    public static final int DIGITO_VERIFICADOR = 8;
 
     public static String visualizarRg(String rg) {
         String rgPadrao = "";
-        
+
         if (rg.length() == 9) {
-            rgPadrao += rg.charAt(PRIMEIRO_DIGITO_1);
-            rgPadrao += rg.charAt(PRIMEIRO_DIGITO_2);
+            rgPadrao += rg.charAt(INDEX_0);
+            rgPadrao += rg.charAt(INDEX_1);
             rgPadrao += ".";
-            rgPadrao += rg.charAt(SEGUNDO_DIGITO_1);
-            rgPadrao += rg.charAt(SEGUNDO_DIGITO_2);
-            rgPadrao += rg.charAt(SEGUNDO_DIGITO_3);
+            rgPadrao += rg.charAt(INDEX_2);
+            rgPadrao += rg.charAt(INDEX_3);
+            rgPadrao += rg.charAt(INDEX_4);
             rgPadrao += ".";
-            rgPadrao += rg.charAt(TERCEIRO_DIGITO_1);
-            rgPadrao += rg.charAt(TERCEIRO_DIGITO_2);
-            rgPadrao += rg.charAt(TERCEIRO_DIGITO_3);
+            rgPadrao += rg.charAt(INDEX_5);
+            rgPadrao += rg.charAt(INDEX_6);
+            rgPadrao += rg.charAt(INDEX_7);
             rgPadrao += "-";
-            rgPadrao += rg.charAt(DIGITO_VERIFICADOR);
+            rgPadrao += rg.charAt(INDEX_8);
         } else {
-            rgPadrao += rg.charAt(PRIMEIRO_DIGITO_1);
-            rgPadrao += rg.charAt(PRIMEIRO_DIGITO_2);
+            rgPadrao += rg.charAt(INDEX_0);
+            rgPadrao += rg.charAt(INDEX_1);
             rgPadrao += ".";
-            rgPadrao += rg.charAt(SEGUNDO_DIGITO_1);
-            rgPadrao += rg.charAt(SEGUNDO_DIGITO_2);
-            rgPadrao += rg.charAt(SEGUNDO_DIGITO_3);
+            rgPadrao += rg.charAt(INDEX_2);
+            rgPadrao += rg.charAt(INDEX_3);
+            rgPadrao += rg.charAt(INDEX_4);
             rgPadrao += ".";
-            rgPadrao += rg.charAt(TERCEIRO_DIGITO_1);
-            rgPadrao += rg.charAt(TERCEIRO_DIGITO_2);
-            rgPadrao += rg.charAt(TERCEIRO_DIGITO_3);
+            rgPadrao += rg.charAt(INDEX_5);
+            rgPadrao += rg.charAt(INDEX_6);
+            rgPadrao += rg.charAt(INDEX_7);
         }
         return rgPadrao;
     }
 
     public static String visualizarCpf(String cpf) {
         String cpfPadrao = "";
-        
-        cpfPadrao += cpf.charAt(0);
-        cpfPadrao += cpf.charAt(1);
-        cpfPadrao += cpf.charAt(2);
+
+        cpfPadrao += cpf.charAt(INDEX_0);
+        cpfPadrao += cpf.charAt(INDEX_1);
+        cpfPadrao += cpf.charAt(INDEX_2);
         cpfPadrao += ".";
-        cpfPadrao += cpf.charAt(3);
-        cpfPadrao += cpf.charAt(4);
-        cpfPadrao += cpf.charAt(5);
+        cpfPadrao += cpf.charAt(INDEX_3);
+        cpfPadrao += cpf.charAt(INDEX_4);
+        cpfPadrao += cpf.charAt(INDEX_5);
         cpfPadrao += ".";
-        cpfPadrao += cpf.charAt(6);
-        cpfPadrao += cpf.charAt(7);
-        cpfPadrao += cpf.charAt(8);
+        cpfPadrao += cpf.charAt(INDEX_6);
+        cpfPadrao += cpf.charAt(INDEX_7);
+        cpfPadrao += cpf.charAt(INDEX_8);
         cpfPadrao += "-";
-        cpfPadrao += cpf.charAt(9);
-        cpfPadrao += cpf.charAt(10);
-        
+        cpfPadrao += cpf.charAt(INDEX_9);
+        cpfPadrao += cpf.charAt(INDEX_10);
+
         return cpfPadrao;
     }
-    
+
     public static String visualizarCnpj(String cnpj) {
         String cnpjPadrao = "";
-        
+
         cnpjPadrao += cnpj.charAt(0);
         cnpjPadrao += cnpj.charAt(1);
         cnpjPadrao += ".";
@@ -92,13 +96,13 @@ public class Visualizador {
         cnpjPadrao += "-";
         cnpjPadrao += cnpj.charAt(12);
         cnpjPadrao += cnpj.charAt(13);
-        
+
         return cnpjPadrao;
     }
 
     public static String visualizarData(String data) {
         String dataPadrao = "";
-        
+
         dataPadrao += data.charAt(8);
         dataPadrao += data.charAt(9);
         dataPadrao += "/";
@@ -109,45 +113,45 @@ public class Visualizador {
         dataPadrao += data.charAt(1);
         dataPadrao += data.charAt(2);
         dataPadrao += data.charAt(3);
-        
+
         return dataPadrao;
     }
-    
+
     public static String visualizarPeso(float peso) {
         String string = Float.toString(peso);
-        string = string.replaceAll("\\.",",");
+        string = string.replaceAll("\\.", ",");
         string = string + " Kg";
         return string;
     }
-    
+
     public static String visualizarImc(float imc) {
         String string = Float.toString(imc);
-        string = string.replaceAll("\\.",",");
+        string = string.replaceAll("\\.", ",");
         return string;
     }
-    
+
     public static String visualizarGorduraCorporalSemPercent(int gordura) {
         String string = Integer.toString(gordura);
-        string = string.replaceAll("\\.0","");
+        string = string.replaceAll("\\.0", "");
         return string;
     }
-    
+
     public static String visualizarGorduraCorporal(int gordura) {
         String string = Integer.toString(gordura);
         string = string + "%";
         return string;
     }
-    
+
     public static String visualizarAltura(float altura) {
         String string = Float.toString(altura);
-        string = string.replaceAll("\\.",",");
+        string = string.replaceAll("\\.", ",");
         string = string + "m";
         return string;
     }
 
     public static String visualizarPhone(String telefone) {
         String telefonePadrao = "";
-        
+
         telefonePadrao += "(";
         telefonePadrao += telefone.charAt(0);
         telefonePadrao += telefone.charAt(1);
@@ -161,13 +165,13 @@ public class Visualizador {
         telefonePadrao += telefone.charAt(7);
         telefonePadrao += telefone.charAt(8);
         telefonePadrao += telefone.charAt(9);
-        
+
         return telefonePadrao;
     }
 
     public static String visualizarCell(String celular) {
         String celularPadrao = "";
-        
+
         celularPadrao += "(";
         celularPadrao += celular.charAt(0);
         celularPadrao += celular.charAt(1);
@@ -182,13 +186,13 @@ public class Visualizador {
         celularPadrao += celular.charAt(8);
         celularPadrao += celular.charAt(9);
         celularPadrao += celular.charAt(10);
-        
+
         return celularPadrao;
     }
-    
+
     public static String visualizarCep(String cep) {
         String cepPadrao = "";
-        
+
         cepPadrao += cep.charAt(0);
         cepPadrao += cep.charAt(1);
         cepPadrao += cep.charAt(2);
@@ -198,28 +202,26 @@ public class Visualizador {
         cepPadrao += cep.charAt(5);
         cepPadrao += cep.charAt(6);
         cepPadrao += cep.charAt(7);
-        
+
         return cepPadrao;
     }
-    
+
     public static String visualizarSexo(String sexo) {
         String string;
-        if (sexo.equals("f")|| sexo.equals("F")) {
+        if (sexo.equals("f") || sexo.equals("F")) {
             string = "Feminino";
-        }
-        else {
+        } else {
             string = "Masculino";
         }
         return string;
     }
-    
+
     public static String visualizarGerente(boolean gerente) {
         String string;
-        
-        if(gerente) {
+
+        if (gerente) {
             string = "SIM";
-        }
-        else {
+        } else {
             string = "NÃO";
         }
         return string;
